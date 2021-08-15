a42babc87835f0d1ebe07026d0af2c0c25905698
lavnyasharma <lavanya2210sharma@gmaul.com>
2021-08-15 14:37:55 +0530

    toDos list in react js

---
+++ my-app/src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+           Lavanya, a Developer & Designer who
+loves to develop innovating things.
+            <p>I describe myself as a
+A highly motivated, dynamic and versatile
+individual who is always eager to learn new skills.</p>
+        </div>
+    )
+}
